2c8d4832b83801694710bbc44f497305d05b511c
MANDRW <mandrzejewski132@gmail.com>
2024-04-08 22:32:27 +0200

    final main

--- main.cpp
+++ main.cpp
@@ -49,14 +49,13 @@ int main() {
     fstream file;
     Sorter sorting;
 
-    Box tenT(10000);
+    //Box tenT(10000);
     //Box oneH(100000);
     //Box fiveH(500000);
     //Box all;//962903
 
-
     //clock_t start=clock();
-    tenT.download(file,10000);
+    //tenT.download(file,10000);
     //oneH.download(file,100000);
     //fiveH.download(file,500000);
     //nineH.download(file,962903);
@@ -65,16 +64,31 @@ int main() {
     double time=double(end-start)/CLOCKS_PER_SEC;
     cout<<"time: "<<time*1000<<"ms"<<endl;*/
 
-
-    clock_t start=clock();
-    sorting.BucketSort(tenT,1.0,10.0); cout<<"Bucket sort:"<<endl;
+    //clock_t start=clock();
+    //sorting.BucketSort(tenT,1.0,10.0); cout<<"Bucket sort:"<<endl;
     //sorting.MergeSort(tenT,0,tenT.get_size()-1);cout<<"Merge sort:"<<endl;
     //sorting.QuickSort(tenT,0,tenT.get_size()-1);cout<<"Quick sort:"<<endl;
-    clock_t end=clock();
-    double time=double(end-start)/CLOCKS_PER_SEC;
-    cout<<"Rozmiar obiektu:"<<tenT.get_size()<<endl<<"time: "<<time*1000<<"ms"<<endl;
-    cout<<"Avg: "<<avg(tenT,tenT.get_size())<<endl;
-    cout<<"Mediana: "<<mediana(tenT,tenT.get_size());
+    //clock_t end=clock();
+    //double time=double(end-start)/CLOCKS_PER_SEC;
+    //cout<<"Rozmiar obiektu:"<<tenT.get_size()<<endl<<"time: "<<time*1000<<"ms"<<endl;
+    //cout<<"Avg: "<<avg(tenT,tenT.get_size())<<endl;
 
+    int test_size=15;
+    Box test1(test_size);
+    Box test2(test_size);
+    Box test3(test_size);
+    test1.download(file,test_size);
+    test3=test2=test1;
+    cout<<"Tablica przed posortowaniem:"<<endl;
+    printArray(test1,test_size);
+    sorting.QuickSort(test1,0,test1.get_size()-1);
+    sorting.BucketSort(test2,1.0,10.0);
+    sorting.MergeSort(test3,0,test3.get_size()-1);
+    cout<<"Qucksort:"<<endl;
+    printArray(test1,test_size);
+    cout<<"Bucketsort:"<<endl;
+    printArray(test2,test_size);
+    cout<<"Mergesort:"<<endl;
+    printArray(test3,test_size);
     return 0;
 }
